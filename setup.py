51d4c6d9022d3af6f7725b58c9707b5c49670338
Alan Garny <agarny@hellix.com>
2020-04-27 18:48:24 +1200

    Added a setup file.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+# Deploy our package.
+
+setup(
+    author='Alan Garny',
+    author_email='a.garny@auckland.ac.nz',
+    description='A Python package to model Covid-19 using the SIRD model',
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'pandas',
+    ],
+    license='Apache 2.0',
+    name='sird',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/ABI-Covid-19/sird',
+    version='0.1.0',
+)
